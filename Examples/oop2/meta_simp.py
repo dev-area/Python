59c1bee0b20fff25fd8b5b82e989eb2c2a39cc1e
Liran <liranbh@gmail.com>
2018-08-12 09:33:25 +0300

    add

--- Examples/oop2/meta_simp.py
+++ Examples/oop2/meta_simp.py
@@ -8,14 +8,44 @@ Created on Tue May 22 21:55:13 2018
 
 class A(type):
     def __call__(c,arg):
-        print("A")
+        c.xyz=900
+        print("A",arg)
+        return super(A,c).__call__(arg)
 
 class B(object):
     def __call__(c,arg):
         print("B")
+    def fn(self):
+        print("B fn")
 
 class C(B,metaclass=A):
     def __call__(c,arg):
         print("C")
+    def __init__(self,x):
+        print("C init",x)
+    def pr(self):
+        print
+
+     
 
 c1= C(10)
+
+
+
+
+
+
+def cldec(ccls):
+     class Inner(ccls):
+         attr = 1008
+     return Inner
+
+@cldec
+class X:
+    def f1(self,v1):
+        print("f1")
+        self.v=v1
+    def f2(self):
+        print(self.v)
+
+
